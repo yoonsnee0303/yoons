f0c3a8681466c8dae4f508cad99f3af06afd18d2
yoonsnee0303 <127804620+yoonsnee0303@users.noreply.github.com>
2023-04-09 13:43:42 +0900

    Update 도매꾹 API_getItemView.py

--- 도매꾹 API_getItemView.py
+++ 도매꾹 API_getItemView.py
@@ -27,84 +27,86 @@ data = json.loads(res.content)
 with open('test.json', 'wb') as f:
     f.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
 
-# #Option askii to Korean
-# data['domeggook']['selectOpt'] = eval(data['domeggook']['selectOpt'].replace('\\"','"'))
+#Option askii to Korean
+#Option askii to Korean
 
-# #otherItem delete
-# del data['domeggook']['desc']['contents']['otherItem']
+data['domeggook']['selectOpt'] = eval(data['domeggook']['selectOpt'].replace('\\"','"'))
 
-# # Saving JSON data to a file
-# with open('test.json', 'wb') as f:
-#     f.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
+#otherItem delete
+del data['domeggook']['desc']['contents']['otherItem']
 
-
-# import pandas as pd
-
-# # read data from excel file
-# dom_title_sum = pd.read_excel(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\dom_title_sum.xlsx')
-
-# # extract header row as list
-# header_list = list(dom_title_sum.columns)
+# Saving JSON data to a file
+with open('test.json', 'wb') as f:
+    f.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
 
 
+import pandas as pd
 
-# #1:1매칭 key:value data만 새로 new_dict만들기
-# def get_all_keys(d):
-#     result = {}
-#     for k, v in d.items():
-#         if isinstance(v, dict):
-#             nested_result = get_all_keys(v)
-#             result.update(nested_result)
-#         else:
-#             if not isinstance(k, dict) and not isinstance(v, dict):
-#                 result[k] = v
-#     return result
-# new_dict = get_all_keys(data)
+# read data from excel file
+dom_title_sum = pd.read_excel(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\dom_title_sum.xlsx')
+
+# extract header row as list
+header_list = list(dom_title_sum.columns)
 
 
 
-# #붙여넣을 data 생성 1row
-# row_list_value = []
-
-# switch = False
-# for head in header_list:
-#     for k, v in new_dict.items():
-#         if head.count(k) > 0:
-#             row_list_value.append(v)
-#             switch = True
-#             break
-#     if switch == False:
-#         row_list_value.append("")
-#     elif switch == True:
-#         switch = False
+#1:1매칭 key:value data만 새로 new_dict만들기
+def get_all_keys(d):
+    result = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            nested_result = get_all_keys(v)
+            result.update(nested_result)
+        else:
+            if not isinstance(k, dict) and not isinstance(v, dict):
+                result[k] = v
+    return result
+new_dict = get_all_keys(data)
 
 
-# import openpyxl as xls
 
-# # for num in range(1,len(row_list_value)+1):
-# #     ws.cell(row=5,column=num,value=row_list_value[num-1]) 
+#붙여넣을 data 생성 1row
+row_list_value = []
 
-# wb = xls.load_workbook(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\dom_title_sum.xlsx')
-# ws = wb.active
-# # get the last row number
-# last_row = ws.max_row + 1
+switch = False
+for head in header_list:
+    for k, v in new_dict.items():
+        if head.count(k) > 0:
+            row_list_value.append(v)
+            switch = True
+            break
+    if switch == False:
+        row_list_value.append("")
+    elif switch == True:
+        switch = False
 
-# # insert values into cells
-# column = 1
 
-# for value in row_list_value:
-#     if isinstance(value, list): # value가 list인지 확인
-#         value = ", ".join(map(str,value)) # 리스트들 안에 있는 원소들을 있는 쉼표로 구분하여 출력
-#     ws.cell(row=5, column=column).value = value
-#     column += 1
+import openpyxl as xls
 
-# # fixed values into cells
-# ws['B5'].value = int(10)
-# ws['AH5'].value = int(10)
+# for num in range(1,len(row_list_value)+1):
+#     ws.cell(row=5,column=num,value=row_list_value[num-1]) 
 
-# # save a modified file
-#     # wb.save(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\test2.xlsx')
-# print(data)
+wb = xls.load_workbook(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\dom_title_sum.xlsx')
+ws = wb.active
+# get the last row number
+last_row = ws.max_row + 1
+
+# insert values into cells
+column = 1
+
+for value in row_list_value:
+    if isinstance(value, list): # value가 list인지 확인
+        value = ", ".join(map(str,value)) # 리스트들 안에 있는 원소들을 있는 쉼표로 구분하여 출력
+    ws.cell(row=5, column=column).value = value
+    column += 1
+
+# fixed values into cells
+ws['B5'].value = int(10)
+ws['AH5'].value = int(10)
+
+# save a modified file
+    # wb.save(r'C:\Users\Data2\OneDrive\바탕 화면\윤\코드\test2.xlsx')
+print(data)
 
 
 
